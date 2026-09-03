6718fc245289340deb626d874833fa466083a89f
logan-truong9 <logantruong@gmail.com>
2026-09-02 19:11:49 -0500

    Histogram Lab

--- u02a_arraylists/labs/04c - Histogram/Histogram.java
+++ u02a_arraylists/labs/04c - Histogram/Histogram.java
@@ -19,37 +19,71 @@ public class Histogram
 
 	public Histogram()
 	{
-      
+      count = new ArrayList<Integer>();
+      letters = new ArrayList<Character>();
+      fileName = "";
 	}
 
 	public Histogram(char[] values, String fName)
 	{
-		out.println("search letters = "+letters);
+      count = new ArrayList<Integer>();
       letters = new ArrayList<Character>();
       fileName = fName;
-      for (int i = 0; i < values.length; ++i) {
-         letters.add(values[i]);
+		
+      
+      for (char c : values) {
+         letters.add(c);
+         count.add(0);
       }
+      
+      out.println("search letters = "+letters);
 	}
 
 	public void loadAndAnalyzeFile() throws IOException
 	{
-      Scanner scan = new Scanner(new File("lab04c1.dat"));
+      Scanner scan = new Scanner(new File(fileName));
       while (scan.hasNextLine()) {
          String line = scan.nextLine();
-         
-         for (
+         for (int i = 0; i < line.length(); ++i) {
+            char c = line.charAt(i);
+            int index = letters.indexOf(c);
+            if (index!=-1) {
+               count.set(index, count.get(index) + 1);
+            }
+         }
+      }
+      scan.close();
 	}
 
 	public char mostFrequent()
 	{
-      for (int i = 0; i < f
-		return '#';
+      if (count.isEmpty()) {
+         return '#';
+      }
+      
+      int maxIndex = 0;
+      for (int i = 1; i < count.size(); ++i) {
+         if (count.get(i) > count.get(maxIndex)) {
+            maxIndex = i;
+         }
+      }
+      return letters.get(maxIndex);
 	}
 
 	public char leastFrequent()
 	{
-		return '#';
+		if (count.isEmpty()) {
+         return '#';
+      }
+      
+      int minIndex = 0;
+      for (int i = 1; i < count.size(); ++i) {
+         if (count.get(i) < count.get(minIndex)) {
+            minIndex = i;
+         }
+      }
+      return letters.get(minIndex);
+
 	}
 
 	public String toString()
